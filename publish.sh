8fdc8fb76f92f619e194a468ce05e63c9ba70efc
pabloFuente <pablofuenteperez@gmail.com>
2024-11-20 01:21:06 +0100

    publish.sh script to publish to NuGet

---
+++ publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [ -z "$NUGET_API_KEY" ]; then
+    echo "Set env var NUGET_API_KEY"
+    exit 1
+fi
+
+SCRIPT_PATH=$(realpath "$0")
+SCRIPT_FOLDER="$(dirname "$SCRIPT_PATH")"
+
+source "$SCRIPT_FOLDER"/build-local.sh || exit 1
+
+pushd "$SCRIPT_FOLDER"/LivekitApi || exit 1
+dotnet pack -c Release || exit 1
+
+pushd "$SCRIPT_FOLDER"/LivekitApi/bin/Release || exit 1
+
+FILENAME_WILDCARD=(Livekit.Server.Sdk.Dotnet.*.nupkg)
+if [ ${#FILENAME_WILDCARD[@]} -ne 1 ]; then
+    echo "Expected one nupkg, got ${#FILENAME_WILDCARD[@]}"
+    exit 1
+fi
+FILENAME=${FILENAME_WILDCARD[0]}
+
+dotnet nuget push "$FILENAME" --api-key "$NUGET_API_KEY" --source https://api.nuget.org/v3/index.json
